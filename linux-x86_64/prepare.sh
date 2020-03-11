67c1c5eb13f23a024630479fcd72c1133ff6ca0d
Edoardo Morandi <morandidodo@gmail.com>
2020-03-11 14:21:47 +0100

    Add prepare.sh
    With this script it is easy to load a basic set of env vars to run
    c2rust

---
+++ linux-x86_64/prepare.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+set -e
+
+BIN_DIR=$(realpath $(dirname $0))
+if [ "x$(command -v clang)" != "x" ]; then
+  CC=clang
+elif [ "x$(command -v gcc)" != "x" ] ; then
+  CC=gcc
+else
+  echo "Clang or GCC are needed"
+  exit -1
+fi
+
+INC_DIRS=$($CC -xc -E -Wp,-v /dev/null 2>&1 | tr '\n' ' ' | sed 's/.*#include <\.\.\.> search starts here://' | sed 's/End of search list.*//' | sed -E 's/^\s*//' | sed -E 's/\s*$//' | sed -E 's/\s+/:/g')
+
+if [ "x$C_INCLUDE_PATH" = "x" ]; then
+  C_INCLUDE_PATH="$INC_DIRS"
+else
+  C_INCLUDE_PATH="$C_INCLUDE_PATH:$INC_DIRS"
+fi
+
+if [ "x$LD_LIBRARY_PATH" = "x"]; then
+  LD_LIBRARY_PATH="$BIN_DIR"
+else
+  LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$BIN_DIR"
+fi
+
+ENV_FILE=$BIN_DIR/env.sh
+cat > $ENV_FILE <<EOF
+export C_INCLUDE_PATH=$C_INCLUDE_PATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH
+EOF
+
+echo -e "Now load the env variable running:\n\nsource '$ENV_FILE'"
